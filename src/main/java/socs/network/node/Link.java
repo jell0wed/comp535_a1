33226425e604340e3bb58e90c94b745aabc69b5d
Jeremie <jeremie@weboxigen.com>
2018-01-30 19:01:47 -0500

    initial stuff

--- src/main/java/socs/network/node/Link.java
+++ src/main/java/socs/network/node/Link.java
@@ -1,12 +1,31 @@
 package socs.network.node;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.Socket;
+
 public class Link {
+    private static final Logger LOG = LoggerFactory.getLogger(Link.class);
 
-  RouterDescription router1;
-  RouterDescription router2;
+    private Socket clientSock;
+    RouterDescription fromRouter;
+    RouterDescription toRouter;
 
-  public Link(RouterDescription r1, RouterDescription r2) {
-    router1 = r1;
-    router2 = r2;
-  }
+    public Link(RouterDescription from, RouterDescription to) {
+        fromRouter = from;
+        toRouter = to;
+
+        this.initializeSocket();
+    }
+
+    private void initializeSocket() {
+        try {
+            this.clientSock = new Socket(this.toRouter.processIPAddress, this.toRouter.processPortNumber);
+            LOG.debug("Initialized client socket to {}", this.toRouter);
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to initializeSocket()", e);
+        }
+    }
 }
--- src/main/java/socs/network/node/RouterDescription.java
+++ src/main/java/socs/network/node/RouterDescription.java
@@ -1,11 +1,26 @@
 package socs.network.node;
 
 public class RouterDescription {
-  //used to socket communication
-  String processIPAddress;
-  short processPortNumber;
-  //used to identify the router in the simulated network space
-  String simulatedIPAddress;
-  //status of the router
-  RouterStatus status;
+    //used to socket communication
+    String processIPAddress;
+    int processPortNumber;
+    //used to identify the router in the simulated network space
+    String simulatedIPAddress;
+    //status of the router
+    RouterStatus status;
+
+
+    @Override
+    public String toString() {
+        return new StringBuilder()
+                .append("Router<")
+                .append(this.processIPAddress)
+                .append(":")
+                .append(this.processPortNumber)
+                .append(">")
+                .append("(")
+                .append(this.simulatedIPAddress)
+                .append(")")
+                .toString();
+    }
 }
