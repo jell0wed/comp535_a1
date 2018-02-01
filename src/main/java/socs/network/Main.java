35794cb3acf85dc276d0ee093ef866dd7f09c830
Jeremie <jeremie@weboxigen.com>
2018-02-01 16:14:54 -0500

    fix

--- src/main/java/socs/network/Main.java
+++ src/main/java/socs/network/Main.java
@@ -5,13 +5,13 @@ import socs.network.util.Configuration;
 
 public class Main {
 
-  public static void main(String[] args) {
-    if (args.length != 2) {
-      System.out.println("usage: program conf_path");
-      System.exit(1);
-    }
-    Router r = new Router(Integer.parseInt(args[0]), args[1]);
-    r.terminal();
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("usage: <physical ip address> <physical port> <simulated ip> <weight>");
+            System.exit(1);
+        }
 
-  }
+        Router r = new Router(Integer.parseInt(args[0]), args[1]);
+        r.terminal();
+    }
 }
